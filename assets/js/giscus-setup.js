a0c407a54c72d8626f078c8708430cc0d42385a8
AbNeil <AbNeil@users.noreply.github.com>
2026-03-06 14:15:33 +0000

    Deploying to gh-pages from @ AbNeil/AbNeil.github.io@9e2becfc6eca4976e408bb862f108ba770587b63 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AbNeil/abdul.github.io",
+    "data-repo": "AbNeil/AbNeil.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
