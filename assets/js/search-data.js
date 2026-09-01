5b1736d74f7e4b95cb10df9d89b19e798dc256f2
AbNeil <27285166+AbNeil@users.noreply.github.com>
2026-09-01 21:28:21 +0000

    Deploying to gh-pages from @ AbNeil/AbNeil.github.io@955c6584c9c53810a9b5181a5aaa39db13f6c1ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "",
+          description: "Curriculum vitae",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
